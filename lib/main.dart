f1d7fec3705f330d1c9786666d1e2e989f79b3b5
Kim Sangyeob <yeob32@gmail.com>
2021-07-18 23:35:28 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    // child: Text(
-    //   'Hello, world !!',
-    //   textDirection: TextDirection.ltr,
-    // ),
-    child: Icon(Icons.star, color: Colors.red[500]),
-  ));
-  // runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -27,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
